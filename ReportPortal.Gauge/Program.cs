ee97dc7ba9f3dd5e2f8065d2367defbe0fbc68e7
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2018-09-20 18:21:20 +0300

    Initial commit

---
+++ ReportPortal.Gauge/Program.cs
@@ -0,0 +1,35 @@
+﻿using Gauge.CSharp.Core;
+using Gauge.Messages;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ReportPortal.Gauge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = Convert.ToInt32(Environment.GetEnvironmentVariable("plugin_connection_port"));
+
+            using (var gaugeConnection = new GaugeConnection(new TcpClientWrapper(port)))
+            {
+                while (gaugeConnection.Connected)
+                {
+                    var message = Message.Parser.ParseFrom(gaugeConnection.ReadBytes().ToArray());
+
+                    if (message.MessageType == Message.Types.MessageType.SuiteExecutionResult)
+                    {
+                        Console.Write("Finishing to send results to Report Portal... ");
+                        Console.WriteLine("Done.");
+
+                        var suiteResult = message.SuiteExecutionResult.SuiteResult;
+                    }
+
+                    if (message.MessageType == Message.Types.MessageType.KillProcessRequest)
+                        return;
+                }
+            }
+        }
+    }
+}
